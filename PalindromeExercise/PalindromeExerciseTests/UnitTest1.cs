3105cf9c2f7a8e72ec824ca78b080a24ce049d9b
Mosby4 <rufus.hamby@gmial.com>
2023-10-17 10:39:26 -0500

    completed

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData( "racecar", true)]
+        [InlineData("level", true)]
+        [InlineData("civic", true)]
+        [InlineData("Rufus", false)]
+        public void paliTest(string word, bool expected)
         {
-
+            //arrange
+            var tester = new WordSmith();
+            //Act
+            var actual = tester.isAPalindrome(word);
+            //assert
+            Assert.Equal(actual, expected);
         }
+        
+       
     }
 }
--- PalindromeExercise/PalindromeExerciseTests/WordSmith.cs
+++ PalindromeExercise/PalindromeExerciseTests/WordSmith.cs
@@ -1,9 +1,27 @@
 ﻿namespace PalindromeExerciseTests
 {
-    internal class WordSmith
+    public class WordSmith
     {
-        public WordSmith()
+        public bool isAPalindrome(string value)
         {
+            int min = 0;
+            int max = value.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = value[min];
+                char b = value[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
         }
+
     }
 }
